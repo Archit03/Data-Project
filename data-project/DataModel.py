eaaa95dcef0770b1338f9e7d2d1344005527e6ff
Archit <architsood99@hotmail.com>
2023-11-26 12:54:37 +0530

    First commit.

---
+++ data-project/DataModel.py
@@ -0,0 +1,33 @@
+import psycopg2 as psy
+import logging
+
+
+def Connection():
+    global connect
+    try:
+        connect = psy.connect("host=127.0.0.1 dbname=postgres user=postgres password=hp14b7860")
+        return connect
+    except psy.Error as e:
+        print("Error couldn't make a connection to the PostgresSQL database")
+        logging.basicConfig(filename='error_log.txt', level=logging.ERROR,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(f'Error connecting to PostgresSQL: {e}')
+
+        return None
+
+
+def DB():
+    conn = Connection()
+    if conn:
+        # Perform operations with the PostgresSQL connection
+        # For example, create a cursor and execute queries
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM your_table")
+        result = cursor.fetchall()
+        print(result)
+        cursor.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    DB()  # calls the DB function.
