6cc30d389ac343bee933d0c71cdf61350555ec74
Archit <architsood99@hotmail.com>
2023-11-26 15:22:32 +0530

    new.

--- data-project/DataModel.py
+++ data-project/DataModel.py
@@ -4,10 +4,10 @@ import logging
 
 def Connection():
     try:
-        connect = psy.connect(host="127.0.0.1", dbname="postgres", user="postgres", password="hp14b7860")
+        connect = psy.connect(host="127.0.0.1", dbname="postgres", user="postgres",
+                              password="hp14b7860")
         print("Successfully connected to the PostgreSQL database")
-        # Set autocommit to True to avoid running the CREATE DATABASE inside a transaction
-        connect.set_session(autocommit=True)
+        connect.set_session(autocommit=True)  # Set autocommit to True to avoid transaction issues
         return connect
     except psy.Error as e:
         print("Error couldn't make a connection to the PostgreSQL database")
@@ -17,31 +17,69 @@ def Connection():
         return None
 
 
+def create_table(conn):
+    try:
+        cursor = conn.cursor()
+
+        # Example: Create a table
+        create_table_query = '''
+            CREATE TABLE IF NOT EXISTS student_info (
+                student_id SERIAL PRIMARY KEY,
+                name VARCHAR(255),
+                age INTEGER,
+                gender VARCHAR(10),
+                subject VARCHAR(255),
+                marks FLOAT
+            );
+        '''
+        cursor.execute(create_table_query)
+
+        print("Table created successfully")
+        logging.info("Table created successfully")
+
+        cursor.close()
+    except psy.Error as e:
+        print("Error executing SQL query")
+        logging.basicConfig(filename='QueryError_log.txt', level=logging.ERROR,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(f'Error executing SQL query: {e}')
+
+
+def add_data_to_table(conn, table_name, data):
+    try:
+        cursor = conn.cursor()
+
+        # Example: Insert data into a table
+        insert_query = f"INSERT INTO {table_name} (name, age, gender, subject, marks) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_query, (data['name'], data['age'], data['gender'], data['subject'], data['marks']))
+
+        print("Data added to the table successfully")
+        logging.info("Data added to the table: %s", data)
+
+        cursor.close()
+    except psy.Error as e:
+        print("Error executing SQL query")
+        logging.basicConfig(filename='QueryError_log.txt', level=logging.ERROR,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(f'Error executing SQL query: {e}')
+
+
 def DB():
     conn = Connection()
     if conn:
         try:
+            # Create the table if it doesn't exist
+            create_table(conn)
+
+            # Take the table name as input (you can modify this based on your use case)
+            table_name = "student_info"
+
+            # Sample data to be added to the table
+            data_to_add = {'name': 'John Doe', 'age': 20, 'gender': 'Male', 'subject': 'Math', 'marks': 85.5}
+
             # Perform operations with the PostgreSQL connection
-            cursor = conn.cursor()
+            add_data_to_table(conn, table_name, data_to_add)
 
-            # Check if the database exists before attempting to create it
-            cursor.execute("SELECT datname FROM pg_database WHERE datname='STUDENT_INFO'")
-            result = cursor.fetchone()
-
-            if result is None:
-                # Database does not exist, so create it
-                cursor.execute("CREATE DATABASE STUDENT_INFO")
-                print("Database STUDENT_INFO created successfully")
-                logging.info("Database STUDENT_INFO created successfully")
-            else:
-                print("Database STUDENT_INFO already exists")
-
-            cursor.close()
-        except psy.Error as e:
-            print("Error executing SQL query")
-            logging.basicConfig(filename='QueryError_log.txt', level=logging.ERROR,
-                                format='%(asctime)s - %(levelname)s - %(message)s')
-            logging.error(f'Error executing SQL query: {e}')
         finally:
             conn.close()
 
