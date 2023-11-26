7b01ded6ea1474d4b3d1cf8ee2aaef9f53d04448
Archit <architsood99@hotmail.com>
2023-11-26 13:16:24 +0530

    Global Variable Usage:
    The use of a global variable (connect) in your Connection function is generally discouraged. Global variables can lead to confusion and make the code less modular and harder to understand.
    Connection and Cursor Handling:
    
    The Connection function was attempting to create a cursor (cur = connect.cursor()) but wasn't using it or returning it. In the corrected version, I removed the unnecessary cursor creation and handled it directly in the DB function where needed.
    Cursor Autocommit Setting:
    
    The autocommit=True setting for the cursor should be applied to the cursor itself, not the connection. In the corrected version, I removed the attempt to set autocommit on the cursor as it is not necessary for this use case.
    Database Creation:
    
    In the DB function, I added IF NOT EXISTS to the CREATE DATABASE statement to avoid an error if the database already exists.
    Committing Changes:
    
    After executing the CREATE DATABASE query, I added conn.commit() to ensure the changes are committed to the database.
    Error Handling:
    
    I improved error handling by catching specific exceptions and logging detailed error messages to the error_log.txt and QueryError_log.txt files.
    Minor Fixes:
    
    Corrected the print messages for better consistency.

--- data-project/DataModel.py
+++ data-project/DataModel.py
@@ -8,34 +8,41 @@ def Connection():
         connect = psy.connect("host=127.0.0.1 dbname=postgres user=postgres password=hp14b7860")
         return connect
     except psy.Error as e:
-        print("Error couldn't make a connection to the PostgresSQL database")
+        print("Error couldn't make a connection to the PostgreSQL database")
         logging.basicConfig(filename='error_log.txt', level=logging.ERROR,
                             format='%(asctime)s - %(levelname)s - %(message)s')
-        logging.error(f'Error connecting to PostgresSQL: {e}')
-    try:
-        cur = connect.cursor()
-    except psy.Error as e:
-        print("Error couldn't make a connection to the PostgresSQL database")
-        logging.basicConfig(filename='CursorError_log.txt', level=logging.ERROR,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
-        logging.error(f'Error connecting to Cursor: {e}')
-        connect.set_session(autocommit=True)
+        logging.error(f'Error connecting to PostgreSQL: {e}')
         return None
 
 
 def DB():
     conn = Connection()
     if conn:
-        # Perform operations with the PostgresSQL connection
-        # For example, create a cursor and execute queries
-        cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE STUDENT_INFO")
-        cursor.execute("SELECT * FROM your_table")
-        result = cursor.fetchall()
-        print(result)
-        cursor.close()
-        conn.close()
+        try:
+            # Perform operations with the PostgreSQL connection
+            cursor = conn.cursor()
+
+            # Example: Create a database (ensure you have the necessary privileges)
+            cursor.execute("CREATE DATABASE IF NOT EXISTS STUDENT_INFO")
+
+            # Commit changes to the database
+            conn.commit()
+
+            # Example: Select data from a table (replace 'your_table' with an actual table name)
+            cursor.execute("SELECT * FROM your_table")
+            result = cursor.fetchall()
+            print(result)
+
+            cursor.close()
+        except psy.Error as e:
+            print("Error executing SQL query")
+            logging.basicConfig(filename='QueryError_log.txt', level=logging.ERROR,
+                                format='%(asctime)s - %(levelname)s - %(message)s')
+            logging.error(f'Error executing SQL query: {e}')
+        finally:
+            conn.close()
 
 
 if __name__ == "__main__":
-    DB()  # calls the DB function.
+    DB()  # calls the DB function. Just fix errors, don't change the code.
+
