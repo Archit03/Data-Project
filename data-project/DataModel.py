b2d4f614d770fc73d077e61d734c0e6357c38295
Archit <architsood99@hotmail.com>
2023-11-26 14:55:55 +0530

    Commit.

--- data-project/DataModel.py
+++ data-project/DataModel.py
@@ -1,10 +1,9 @@
 import psycopg2 as psy
 import logging
 
-
 def Connection():
     try:
-        connect = psy.connect(host="127.0.0.1", dbname="postgres", user="postgres", password="hp14b7860")
+        connect = psy.connect(host="127.0.0.1", port=1024, dbname="postgres", user="postgres", password="hp14b7860")
         print("Successfully connected to the PostgreSQL database")
         # Set autocommit to True to avoid running the CREATE DATABASE inside a transaction
         connect.set_session(autocommit=True)
@@ -16,7 +15,6 @@ def Connection():
         logging.error(f'Error connecting to PostgreSQL: {e}')
         return None
 
-
 def DB():
     conn = Connection()
     if conn:
@@ -24,18 +22,17 @@ def DB():
             # Perform operations with the PostgreSQL connection
             cursor = conn.cursor()
 
-            # Example: Create a database if it does not exist
-            cursor.execute("CREATE DATABASE IF NOT EXISTS STUDENT_INFO")
-
-            # Check if the database was created successfully
+            # Check if the database exists before attempting to create it
             cursor.execute("SELECT datname FROM pg_database WHERE datname='STUDENT_INFO'")
             result = cursor.fetchone()
-            if result:
+
+            if result is None:
+                # Database does not exist, so create it
+                cursor.execute("CREATE DATABASE STUDENT_INFO")
                 print("Database STUDENT_INFO created successfully")
                 logging.info("Database STUDENT_INFO created successfully")
             else:
-                print("Database creation failed")
-                logging.error("Database creation failed")
+                print("Database STUDENT_INFO already exists")
 
             cursor.close()
         except psy.Error as e:
@@ -45,7 +42,6 @@ def DB():
             logging.error(f'Error executing SQL query: {e}')
         finally:
             conn.close()
-
 
 if __name__ == "__main__":
     DB()  # calls the DB function.
