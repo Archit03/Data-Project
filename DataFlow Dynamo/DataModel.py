86ddf1a0712aecfdf5849222da3054b6be9e1888
Archit Sood <architsood99@hotmail.com>
2023-12-08 09:39:37 +0530

    Merge remote-tracking branch 'origin/main'.

--- DataFlow Dynamo/DataModel.py
+++ DataFlow Dynamo/DataModel.py
@@ -1,133 +1,112 @@
 import psycopg2 as psy
 import logging
 
+# Configure logging once at the beginning
+logging.basicConfig(filename='error_log.txt', level=logging.ERROR,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-def Connection():
+
+def connect_to_postgres():
     try:
-        connect = psy.connect(host="127.0.0.1", dbname="postgres", user="postgres", password="root")
-        if connect:
-            print("Successfully connected to the PostgreSQL database")
-        else:
-            print("An error occurred, please check the logs.")
-        connect.set_session(autocommit=True)  # Set autocommit to True to avoid transaction issues
-        return connect
-    except psy.Error as e:
-        print("Error connecting to PostgreSQL database")
-        logging.basicConfig(filename='error_log.txt', level=logging.ERROR,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
-        logging.error(f'Error connecting to PostgreSQL: {e}')
-        return None
-
-
-def create_database(conn, database_name):
-    cursor = conn.cursor()
-    try:
-        # Connect to the default PostgreSQL database (usually 'postgres')
-        conn.set_session(autocommit=True)  # Set autocommit to True to avoid transaction issues
-        cursor = conn.cursor()
-
-        # Check if the database already exists
-        cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (database_name,))
-        database_exists = cursor.fetchone() is not None
-
-        if not database_exists:
-            # Create the new database
-            cursor.execute(f"CREATE DATABASE {database_name}")
-            print(f"Database {database_name} created successfully")
-        else:
-            print(f"Database {database_name} already exists")
+        connection = psy.connect(host="127.0.0.1", dbname="postgres", user="postgres", password="root")
+        connection.set_session(autocommit=True)
+        print("Successfully connected to the PostgreSQL database")
+        return connection
 
     except psy.Error as e:
-        print("Error creating the database")
-        logging.basicConfig(filename='error_log.txt', level=logging.ERROR,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
-        logging.error(f'Error creating the database: {e}')
-    finally:
-        if cursor:
-            cursor.close()
+        error_message = "Error connecting to PostgreSQL database"
+        print(error_message)
+        logging.error(f'{error_message}: {e}')
+        raise e
 
 
-def create_Table(conn, table_name, column):
-    cursor = conn.cursor()
+def create_database(connection, database_name):
     try:
-        create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (database_name,))
+            database_exists = cursor.fetchone() is not None
 
-        for col, data_type in column.items():
-            create_table_query += f"{col} {data_type}, "
+            if not database_exists:
+                cursor.execute(f"CREATE DATABASE {database_name}")
+                print(f"Database '{database_name}' created successfully.")
+            else:
+                print(f"Database '{database_name}' already exists")
 
-        create_table_query = create_table_query.rstrip(', ')
-        create_table_query += ");"
-        cursor.execute(create_table_query)
+    except psy.Error as e:
+        error_message = f"Error creating the database '{database_name}': {e}"
+        print(error_message)
+        logging.error(error_message)
+        raise e
 
-        conn.commit()
 
-        print(f"Table '{table_name}' created successfully.")
+def create_table(connection, table_name, columns):
+    try:
+        with connection.cursor() as cursor:
+            create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+
+            for col, data_type in columns.items():
+                create_table_query += f"{col} {data_type}, "
+
+            create_table_query = create_table_query.rstrip(', ')
+            create_table_query += ");"
+
+            cursor.execute(create_table_query)
+            print(f"Table '{table_name}' created successfully.")
 
     except psy.Error as e:
         error_message = f"Error creating table '{table_name}': {e}"
         print(error_message)
-        logging.basicConfig(filename='error_log.txt', level=logging.ERROR,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
         logging.error(error_message)
-        # Raise the exception again to propagate it to the calling code
         raise e
 
-    finally:
-        if cursor:
-            cursor.close()
 
-
-def add_data_to_table(conn, data, table_name):
-    cursor = conn.cursor()
-
+def add_data_to_table(connection, data, table_name):
     try:
-        # Assuming all lists in data have the same length
-        data_length = len(list(data.values())[0])
+        with connection.cursor() as cursor:
+            data_length = len(list(data.values())[0])
 
-        for i in range(data_length):
-            values = [data[column][i] for column in data]
-            placeholders = ', '.join(['%s'] * len(values))
+            for i in range(data_length):
+                values = [data[column][i] for column in data]
+                placeholders = ', '.join(['%s'] * len(values))
 
-            # Construct the INSERT query
-            insert_query = f"INSERT INTO {table_name} ({', '.join(data.keys())}) VALUES ({placeholders});"
+                insert_query = f"INSERT INTO {table_name} ({', '.join(data.keys())}) VALUES ({placeholders});"
+                cursor.execute(insert_query, tuple(values))
 
-            # Execute the query
-            cursor.execute(insert_query, tuple(values))
-            conn.commit()
-
-        print(f"Data added to the table '{table_name}' successfully.")
+            print(f"Data added to the table '{table_name}' successfully.")
 
     except psy.Error as e:
         error_message = f"Error adding data to table '{table_name}': {e}"
         print(error_message)
         logging.error(error_message)
-
-    finally:
-        cursor.close()
+        raise e
 
 
-def query():
-    return None
-
-
-# Example usage
 if __name__ == "__main__":
-    connection = Connection()  # Get a PostgreSQL connection
-    columns = {
-        "Student_ID": "SERIAL PRIMARY KEY",
-        "Student_Age": "INTEGER",
-        "Student_Name": "VARCHAR(255)",
-        "Phone": "BIGINT"
-    }
+    try:
+        postgres_connection = connect_to_postgres()
+        create_database(postgres_connection, "student_data")
 
-    sample_data = {
-        "Student_Age": [25, 24],
-        "Student_Name": ["John Doe", "Archit Sood"],
-        "Phone": [1234567890, 3284883274]
-    }
+        table_columns = {
+            "Student_ID": "SERIAL PRIMARY KEY",
+            "Student_Age": "INTEGER",
+            "Student_Name": "VARCHAR(255)",
+            "Phone": "BIGINT"
+        }
 
-    tablename = "Student"
-    if connection:
-        create_database(connection, "student_data")
-        create_Table(connection, tablename, columns)
-        add_data_to_table(connection, sample_data, tablename)
+        sample_data = {
+            "Student_Age": [25, 24],
+            "Student_Name": ["John Doe", "Archit Sood"],
+            "Phone": [1234567890, 3284883274]
+        }
+
+        table_name = "Student"
+
+        create_table(postgres_connection, table_name, table_columns)
+        add_data_to_table(postgres_connection, sample_data, table_name)
+
+    except psy.Error as e:
+        print("An error occurred.")
+        # Handle the exception as needed.
+    finally:
+        if postgres_connection:
+            postgres_connection.close()
--- DataFlow Dynamo/Tests.py
+++ DataFlow Dynamo/Tests.py
@@ -1,58 +1,61 @@
-
-
-# Import the functions from your code
-from DataModel import Connection, create_database, create_Table, add_data_to_table, query
+from DataModel import connect_to_postgres, create_database, create_table, add_data_to_table
 
 
 def test_connection():
     print("Testing database connection...")
-    connection = Connection()
+    connection = connect_to_postgres()
     assert connection is not None, "Connection failed!"
     print("Connection successful!")
 
 
 def test_create_database():
     print("Testing create database...")
-    connection = Connection()
+    connection = connect_to_postgres()
     create_database(connection, "test_database")
     print("Database creation test successful!")
 
 
 def test_create_table():
     print("Testing create table...")
-    connection = Connection()
+    connection = connect_to_postgres()
     columns = {
         'id': 'SERIAL PRIMARY KEY',
         'name': 'VARCHAR(255)',
         'age': 'INTEGER',
         'gender': 'VARCHAR(10)'
     }
-    create_Table(connection, "test_table", columns)
+    create_table()(connection, "test_table", columns)
     print("Table creation test successful!")
 
 
 def test_add_data_to_table():
     print("Testing add data to table...")
-    connection = Connection()
+    connection = connect_to_postgres()
     table_name = "test_table"
     data = {'name': 'John Doe', 'age': 25, 'gender': 'Male'}
-    add_data_to_table(connection, table_name, data)
+    add_data_to_table(connection, data, table_name)  # Corrected the parameter order
     print("Data addition test successful!")
 
 
 def test_query():
     print("Testing query...")
-    connection = Connection()
-    table_name = "test_table"
-    data = {'name': 'Jane Doe', 'age': 30, 'gender': 'Female'}
-    query("test_database", table_name, data)
-    print("Query test successful!")
+    # Modify this function based on your query testing requirements
+    pass
 
 
 # Run the tests
 if __name__ == "__main__":
-    test_connection()
-    test_create_database()
-    test_create_table()
-    test_add_data_to_table()
-    test_query()
+    try:
+        test_connection()
+        test_create_database()
+        test_create_table()
+        test_add_data_to_table()
+        test_query()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Close the connection after all tests are done
+        connection = connect_to_postgres()
+        if connection:
+            connection.close()
