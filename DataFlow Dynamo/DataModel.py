a212160d81aa4d298b7f4d7e13cb49fda332b135
Archit Sood <architsood99@hotmail.com>
2023-12-08 10:24:09 +0530

    Merge remote-tracking branch 'origin/main'.

--- DataFlow Dynamo/DataModel.py
+++ DataFlow Dynamo/DataModel.py
@@ -39,10 +39,21 @@ def create_database(connection, database_name):
         raise e
 
 
-def create_table(connection, table_name, columns):
+def check_database_existence(connection, database_name):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (database_name,))
+        database_exists = cursor.fetchone() is not None
+        if database_exists:
+            print("Database exists.")
+        else:
+            print("DB doesn't exist.")
+        return database_exists
+
+
+def create_table(connection, table_, columns):
     try:
         with connection.cursor() as cursor:
-            create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+            create_table_query = f"CREATE TABLE IF NOT EXISTS {table_} ("
 
             for col, data_type in columns.items():
                 create_table_query += f"{col} {data_type}, "
@@ -51,16 +62,28 @@ def create_table(connection, table_name, columns):
             create_table_query += ");"
 
             cursor.execute(create_table_query)
-            print(f"Table '{table_name}' created successfully.")
+            print(f"Table '{table_}' created successfully.")
 
     except psy.Error as e:
-        error_message = f"Error creating table '{table_name}': {e}"
+        error_message = f"Error creating table '{table_}': {e}"
         print(error_message)
         logging.error(error_message)
         raise e
 
 
-def add_data_to_table(connection, data, table_name):
+def check_table_existence(connection, table):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = %s);", (table,))
+        table_exists = cursor.fetchone()[0]
+        if table_exists:
+            print("Table exists.")
+
+        else:
+            print("Table doesn't exist.")
+        return table_exists
+
+
+def add_data_to_table(connection, data, tablename):
     try:
         with connection.cursor() as cursor:
             data_length = len(list(data.values())[0])
@@ -69,23 +92,27 @@ def add_data_to_table(connection, data, table_name):
                 values = [data[column][i] for column in data]
                 placeholders = ', '.join(['%s'] * len(values))
 
-                insert_query = f"INSERT INTO {table_name} ({', '.join(data.keys())}) VALUES ({placeholders});"
+                insert_query = f"INSERT INTO {tablename} ({', '.join(data.keys())}) VALUES ({placeholders});"
                 cursor.execute(insert_query, tuple(values))
 
-            print(f"Data added to the table '{table_name}' successfully.")
+            print(f"Data added to the table '{tablename}' successfully.")
 
     except psy.Error as e:
-        error_message = f"Error adding data to table '{table_name}': {e}"
+        error_message = f"Error adding data to table '{tablename}': {e}"
         print(error_message)
         logging.error(error_message)
         raise e
 
 
+# Example usage
 if __name__ == "__main__":
     try:
         postgres_connection = connect_to_postgres()
-        create_database(postgres_connection, "student_data")
+        database_name = "student_data"
+        if not check_database_existence(postgres_connection, database_name):
+            create_database(postgres_connection, database_name)
 
+        table_name = "Student"
         table_columns = {
             "Student_ID": "SERIAL PRIMARY KEY",
             "Student_Age": "INTEGER",
@@ -93,15 +120,15 @@ if __name__ == "__main__":
             "Phone": "BIGINT"
         }
 
+        if not check_table_existence(postgres_connection, table_name):
+            create_table(postgres_connection, table_name, table_columns)
+
         sample_data = {
             "Student_Age": [25, 24],
             "Student_Name": ["John Doe", "Archit Sood"],
             "Phone": [1234567890, 3284883274]
         }
 
-        table_name = "Student"
-
-        create_table(postgres_connection, table_name, table_columns)
         add_data_to_table(postgres_connection, sample_data, table_name)
 
     except psy.Error as e:
